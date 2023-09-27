8e94c81da3afc731a7990901823e692e1e1c0223
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-09-27 14:00:15 +0200

    Complete challenge

---
+++ Challenge/main.js
@@ -0,0 +1,10 @@
+const sentence = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+
+console.log(sentence);
+
+const sentence1 = sentence.split('$');
+
+console.log(sentence1);
+
+console.log(sentence1.join(' '));
+
