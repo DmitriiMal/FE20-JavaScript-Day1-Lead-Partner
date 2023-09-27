080f0169a0b2ec3ce776cbf02117c1282512ab9c
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-09-27 11:59:08 +0200

    complete intermediate exercise

---
+++ Int/main.js
@@ -0,0 +1,70 @@
+// Exercise 1
+
+// let cars = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Mazda', 'Fiat', 'Ferrari'];
+// console.log(cars);
+// let carsAlph = cars.sort();
+// console.log(carsAlph);
+// document.write(carsAlph);
+
+
+// Exercise 2
+// 1
+
+// const fruits = ['apple', 'banana', 'kiwi'];
+
+// console.log(fruits);
+
+// fruits.push('orange');
+
+// console.log(fruits);
+
+// fruits.pop('orange');
+
+// console.log(fruits);
+
+// 2 //
+
+// const animals = ['monkey', 'horse', 'dog'];
+// console.log(animals);
+
+// animals.sort();
+// console.log(animals);
+
+// animals.push('cat');
+// animals.sort();
+// console.log(animals);
+
+
+// Exercise 3
+
+const fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+// document.write(fruits);
+
+const fruitsArr = fruits.split("/");
+// document.write(fruitsArr);
+
+// const mango = fruitsArr[0]
+
+document.write(`${fruitsArr[0]} <br> ${fruitsArr[1]} <br> ${fruitsArr[2]} <br> ${fruitsArr[3]} <br> ${fruitsArr[4]} <br> ${fruitsArr[5]} <br> ${fruitsArr[6]} <br> ${fruitsArr[7]} <br> ${fruitsArr[8]}`);
+
+
+
+// document.write(fruitsArr[0]);
+// document.write(fruitsArr[1]);
+// document.write(fruitsArr[2]);
+// document.write(fruitsArr[3]);
+// document.write(fruitsArr[4]);
+// document.write(fruitsArr[5]);
+// document.write(fruitsArr[6]);
+// document.write(fruitsArr[7]);
+
+
+
+// console.log(fruitsArr[0]);
+// console.log(fruitsArr[1]);
+// console.log(fruitsArr[2]);
+// console.log(fruitsArr[3]);
+// console.log(fruitsArr[4]);
+// console.log(fruitsArr[5]);
+// console.log(fruitsArr[6]);
+// console.log(fruitsArr[7]);
